de4a7c10ae1a4d63b4c79d578479eb304f6ed59f
MemberBuddy <MemberBuddy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-02 10:08:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/framework-api/trunk@432166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ framework-api.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Plugin Framework API
+Plugin URI: http://memberbuddy.com/plugins/framework-api/
+Description: A shared class of functions to be used as a base for creating other plugins
+Author: Rob Holmes
+Author URI: http://memberbuddy.com/people
+Version: 0.0.1
+Tags: Wordpress
+License: GPL2
+
+Copyright 2011  Rob Holmes (email : rob@onemanonelaptop.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+// define the abstract framework class, sadly this isn't enough to make sure its available to other plugins. 
+
+include_once('framework.php');
+?>
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Framework ===
+Contributors: MemberBuddy
+Tags: Wordpress
+Requires at least: 3.0
+Tested up to: 3.0
+
+
+Defines an extensible framework for use by other plugins, by keeping the generic code in one place will allow for easier maintenance across a large set of complex plugins.
+
+== Description ==
+
+Defines an extensible framework for use by other plugins, by keeping the generic code in one place will allow for easier maintenance across a large set of complex plugins.
+
+== Installation ==
+1. Verify that you have PHP5, which is required for this plugin.
+2. Upload the whole `framework` directory to the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.0.1 =
+* Alpha Version
